02c030da331dd40735da193d76f0f7ca8197ee62
Rahul Chaurasia <rahulrchaurasia@gmail.com>
2023-02-25 17:23:17 +0530

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Rahul Chaurasia on 25/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
