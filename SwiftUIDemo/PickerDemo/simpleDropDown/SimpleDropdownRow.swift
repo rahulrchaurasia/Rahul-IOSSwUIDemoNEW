0da9c35f3fb307405e4a44ad7550405812e70f5f
Rahul Chaurasia <rahulrchaurasia@gmail.com>
2024-05-28 17:04:32 +0530

    DropDown sample

---
+++ SwiftUIDemo/PickerDemo/simpleDropDown/SimpleDropdownRow.swift
@@ -0,0 +1,18 @@
+//
+//  SimpleDropdownRow.swift
+//  SwiftUIDemo
+//
+//  Created by Rahul Chaurasia on 24/05/24.
+//
+
+import SwiftUI
+
+struct SimpleDropdownRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SimpleDropdownRow()
+}
