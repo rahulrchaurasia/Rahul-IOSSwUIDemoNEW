bcfc8aa4623c9d93b251143055c7a2fae36d9aea
Rahul Chaurasia <rahulrchaurasia@gmail.com>
2025-07-18 18:00:32 +0530

    git add all changed

--- SwiftUIDemo/CollapsingHeaderView/advanced/StretchyHeaderImageView1.swift
+++ SwiftUIDemo/CollapsingHeaderView/advanced/StretchyHeaderImageView1.swift
@@ -1,5 +1,5 @@
 //
-//  StretchyHeaderImageView.swift
+//  StretchyHeaderImageView1.swift
 //  SwiftUIDemo
 //
 //  Created by Rahul Chaurasia on 18/07/25.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct StretchyHeaderImageView: View {
+struct StretchyHeaderImageView1: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    StretchyHeaderImageView()
+    StretchyHeaderImageView1()
 }
