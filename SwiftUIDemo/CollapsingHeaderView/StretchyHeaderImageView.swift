7ae4f196222e4f6bb35ce02ff60c6c071f53dd66
Rahul Chaurasia <rahulrchaurasia@gmail.com>
2025-07-18 17:30:49 +0530

    Pinned View demo Incomplete

--- SwiftUIDemo/CollapsingHeaderView/StretchyHeaderImageView.swift
+++ SwiftUIDemo/CollapsingHeaderView/StretchyHeaderImageView.swift
@@ -1,18 +1,18 @@
 //
-//  CollapseHeaderView.swift
+//  StretchyHeaderImageView.swift
 //  SwiftUIDemo
 //
-//  Created by Rahul Chaurasia on 16/07/25.
+//  Created by Rahul Chaurasia on 18/07/25.
 //
 
 import SwiftUI
 
-struct CollapseHeaderView: View {
+struct StretchyHeaderImageView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    CollapseHeaderView()
+    StretchyHeaderImageView()
 }
